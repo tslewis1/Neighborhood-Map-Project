f4979bb0cadd61fba2451fe1ddc145d777ca766b
Taryn Lewis <tarynslewis@gmail.com>
2018-09-18 13:53:56 -0700

    Initialize map

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var map;
+
+// Create a new array for all markers
+var markers = [];
+
+function initMap() {
+	// Create styles array for the map
+	var styles = [{
+
+	}];
+
+	// Create a new map
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 37.235808, lng: -121.962375},
+		zoom: 14,
+		styles: styles,
+		mapTypeControl: false
+	});
+
+	var locations = [
+		{title: location:}
+	]
+}
