4a327c1c339050fef7f65d53b7a69588e0106249
Taryn Lewis <tarynslewis@gmail.com>
2018-09-19 14:23:25 -0700

    Create map markers and add them to the map

--- js/app.js
+++ js/app.js
@@ -10,10 +10,28 @@ var mapModel = function() {
 		{title: "Maple Leaf Donuts", location: {lat: 37.233336, lng: -121.978076}},
 		{title: "Nothing Bundt Cakes", location: { lat: 37.229968, lng: -121.981321}}
 	];
+
+	// This for loop uses the bakery locations array to create an array of map markers
+	for (var i = 0; i <bakeryLocations.length; i++) {
+		var position = bakeryLocations[i].location;
+		var title = bakeryLocations[i].title;
+		// Create a marker for each location and put them into an array
+		var marker = new google.maps.Marker({
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			icon: defaultMarker
+		})
+	}
+
+	// Push markers to the array
+	markers.push(marker);
 };
 
-var mapView = {
+var mapView = function() {
 	var map;
+	
+	// Initialize new map
 	initMap: function() {
 		// Create a new map
 		map = new google.maps.Map(document.getElementById('map'), {
@@ -21,9 +39,18 @@ var mapView = {
 			zoom: 14
 		});
 	}
+
+	// Style the default marker icon
+	var defaultMarker = makeMarkers('0091FF');
+
+	// Create a highlighted marker color for when the user clicks on or mouses over the marker
+	var highlightedMarker = makeMarkers('EDF453');
+
+	var infoWindow = new google.maps.InfoWindow();
 };
 
-var viewModel = {
+var viewModel = function() {
 	
 }
+
 mapView.initMap();
