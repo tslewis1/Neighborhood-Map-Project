9a162b48dd25b175afbc632442f304e8f96d9f33
Taryn Lewis <tarynslewis@gmail.com>
2018-09-19 12:49:21 -0700

    Move code into MVVM order

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
 var map;
 
-// Create a new array for all markers
-var markers = [];
-
-function initMap() {
-	// Create a new map
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 37.235808, lng: -121.962375},
-		zoom: 14,
-	});
-
+var mapView = function() {
+	// Create a new array for all markers
+	var markers = [];
 	// These are the bakery places that will be shown to the user
-	var locations = [
+	var bakeryLocations = [
 		{title: "Manresa Bread", location: {lat: 37.227059, lng: -121.981004}},
 		{title: "Fleur De Cocoa", location: {lat: 37.223632, lng: -121.983657}},
 		{title: "Icing On The Cake", location: {lat: 37.222009, lng: -121.982357}},
 		{title: "Mama's Bakery", location: {lat: 37.231465, lng: -121.980132}},
 		{title: "Maple Leaf Donuts", location: {lat: 37.233336, lng: -121.978076}},
 		{title: "Nothing Bundt Cakes", location: { lat: 37.229968, lng: -121.981321}}
-	]
+	];
+};
+
+var mapModel = function() {
+	function initMap() {
+		// Create a new map
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 37.235808, lng: -121.962375},
+			zoom: 14
+		});
+	}
+};
+
+var viewModel = function() {
+
 }
