9a468d125316cf6f5cecaee34399bba9b2eabd43
Taryn Lewis <tarynslewis@gmail.com>
2018-09-25 12:06:10 -0700

    Change marker image and add infowindow content, including streetView

--- js/app.js
+++ js/app.js
@@ -11,6 +11,11 @@ function initMap() {
 		zoom: 15
 	});
 
+	// Create a searchbox to search for places
+	var searchBox = new google.maps.places.SearchBox(
+		document.getElementById('filter-locations-box'));
+	// Keep the search within the map bounds
+	searchBox.setBounds(map.getBounds());
 	// These are the bakery places that will be shown to the user
 	var locations = [
 			{title: "Manresa Bread", location: {lat: 37.227059, lng: -121.981004}},
@@ -21,25 +26,82 @@ function initMap() {
 			{title: "Nothing Bundt Cakes", location: { lat: 37.229968, lng: -121.981321}}
 	];
 
+	// Make infowindow 
 	var infoWindow = new google.maps.InfoWindow();
+
+	// Style the default marker
+	var defaultMarker = 'map-icons/bakery.svg'
+
+	// Created a highlighted marker color for when the user mouses over the marker
+	var highlightedMarker = 'map-icons/bakery-highlighted.psd'
 
     // The following group uses the location array to create an array of markers on initialize.
     for (var i = 0; i < locations.length; i++) {
     	console.log(locations[i].location);
 	    var position = locations[i].location;
 	    var title = locations[i].title;
+	    var image = 'map-icons/bakery.svg';
 	    // Create a marker per location, and put into markers array.
 	    var marker = new google.maps.Marker({
 	    	position: position,
 	        title: title,
 	        animation: google.maps.Animation.DROP,
 	        // icon: defaultMarker,
-	        id: i
+	        id: i,
+	        icon: image
 		});
 
     marker.setMap(map);
-	}
-};
-// const Yelp = require('yelp-fusion');
+	};
 
-// const apiKey = 'B55nUzn2sGNPGNgWRLih9hhH4yT64R4UHllXLdiuYEdPW3lEtriTSdQsOP1Ne-GB-QsCjjzAL-2HIBinde8CddU8OwyolvUr07Vu9MVnG3diFOPCI-f9oQZvCMKiW3Yx'
+	marker.addListener('click', function() {
+		addInfoWindowContent(this, infoWindow);
+	});
+
+	marker.addListener('click', toggleBounce);
+};
+
+ function toggleBounce() {
+ 	if (marker.getAnimation() !== null) {
+ 		marker.setAnimation(null);
+ 	} else {
+ 		marker.setAnimation(google.maps.Animation.BOUNCE);
+ 	}
+ };
+
+function addInfoWindowContent(marker, infowindow) {
+	// Check to see if infowindow is not open
+	if (infowindow.marker != marker) {
+		infowindow.setContent('');
+		infowindow.marker = marker;
+		// Clear the infowindow content when it is closed
+		infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		});
+
+		var streetViewService = new google.maps.StreetViewService();
+		var radius = 40;
+
+		function findStreetView(data, status) {
+			if (status == google.maps.StreetViewStatus.OK) {
+				var nearStreetView = data.location.latLng;
+				var heading = google.maps.geometry.spherical.computeHeading(
+					nearStreetView, marker.position);
+					infowindow.setContent('<div>' + marker.title + '</div><div id = "streetview-pano"></div>');
+					var panoInfo = {
+						position: nearStreetView,
+						pov: {
+							heading: heading,
+							pitch: 25
+						}
+					};
+				var panorama = new google.maps.StreetViewPanorama(
+					document.getElementById('streetview-pano'), panoInfo);
+			} else {
+				infowindow.setContent('<div>' + marker.title + '</div><div>No Street View Found</div>');
+			}
+		}
+		streetViewService.getPanoramaByLocation(marker.position, radius, findStreetView);
+		infowindow.open(map, marker);
+	}
+}
