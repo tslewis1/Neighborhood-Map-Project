e53978f444f13cf0f35bf07f705c1ff5f058a812
Taryn Lewis <tarynslewis@gmail.com>
2018-11-02 13:20:40 -0700

    Update code for bouncing markers and opening/closing infoWindows

--- js/app.js
+++ js/app.js
@@ -27,6 +27,10 @@ function initMap() {
 
   	// autocomplete = new google.maps.places.Autocomplete(input, options);
 
+  	// Foursquare Client ID and Secret 
+	// var clientId = '0K2AW0JW2DVNI2H4VTSQNT0O4IN0WARG2B5GQQMUHSP5WR2J',
+	// var clientSecret = 'RUZEIQTPWI2DGI04FBLNZ5SVBBCZFSGRXGOGEP0LGN4HLPKQ';
+
     // These are the bakery places that will be shown to the user
     var locations = [
             {title: "Manresa Bread", location: {lat: 37.227059, lng: -121.981004}, category: ['bakery', 'bread']},
@@ -36,12 +40,6 @@ function initMap() {
             {title: "Maple Leaf Donuts", location: {lat: 37.233336, lng: -121.978076}, category: ['bakery', 'donuts']},
             {title: "Nothing Bundt Cakes", location: { lat: 37.229968, lng: -121.981321}, category: ['bakery', 'cake']}
     ];
-
-  	// Make infowindow 
-  	var infoWindow = new google.maps.InfoWindow({
-   		content: '<div id="infowindow-content"></div>',
-   		position: position
-  	});
 
   	// The following group uses the location array to create an array of markers on initialize.
   	for (var i = 0; i < locations.length; i++) {
@@ -53,43 +51,44 @@ function initMap() {
     	// Create a marker per location, and put into markers array.
     	marker = new google.maps.Marker({
       	position: position,
+      	map: map,
       	title: title,
       	animation: google.maps.Animation.DROP,
       	icon: image,
       	category: category,
       	id: i
     	});
-    marker.setMap(map);
+    
+    	marker.setMap(map);
 
-    var bouncingMarker = null;
+	  	// Make infowindow 
+	  	var infowindow = new google.maps.InfoWindow({
+	   		content: locations[i].title
+	  	});
 
-    // Add event listener for markers being clicked, and add animation when clicked
-    marker.addListener('click', (function(marker, i) {
-    	return function() {
-        	if (bouncingMarker)
-        		bouncingMarker.setAnimation(null);
-        	if (bouncingMarker != this) {
-        		this.setAnimation(google.maps.Animation.BOUNCE);
-        		bouncingMarker = this;
-        	} else {
-        		bouncingMarker = null;
-        	}
-        infoWindow.setContent(this.title);
-        infoWindow.open(map, this);
-      	};
-    })(this, i));
-	}
+    	// When any marker is clicked, it should bounce. 
+    	// When any marker is clicked, the first marker should stop bouncing.
+    	var bouncingMarker = null;
 
-	google.maps.event.addListener(infoWindow, 'domready', function() {
-		var panorama = new google.maps.StreetViewPanorama(document.getElementById('infowindow-content'));
-		panorama.setPosition(infoWindow.getPosition());
-	});
-}
-
-function filterLocations() {
-	if (marker.category == category || category.length == 0) {
-		marker.setVisible(true);
-	} else {
-		marker.setVisible(false);
+    	// Add event listener for markers being clicked, and add animation when clicked
+	    marker.addListener('click', (function(marker, i) {
+	    	return function() {
+	    		infowindow.open(map, marker);
+	    		// If the clicked marker equals the bouncing marker, the bouncing should stop. 
+	    		// When the clicked marker does not equal the bouncing marker, the clicked marker should bounce
+	    		// and the first bouncing marker should stop bouncing.
+	    		if (bouncingMarker == this) {
+	    			bouncingMarker.setAnimation(null);
+	    			bouncingMarker = null;
+	    			infowindow.close();
+	    		} else {
+	    			this.setAnimation(google.maps.Animation.BOUNCE);
+	    			if (bouncingMarker) {
+		    			bouncingMarker.setAnimation(null);
+	    			};
+	    			bouncingMarker = this;
+	    		};
+	      	};
+	    })(this, i));
 	}
 }
