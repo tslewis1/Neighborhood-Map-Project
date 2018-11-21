328c8e4170108fed0d8e95558e4d346dec61bd18
Ian Campbell <iancam92@icloud.com>
2018-11-21 14:37:40 -0700

    filtering components are ready!

--- js/locations.js
+++ js/locations.js
@@ -38,7 +38,29 @@ const bakeries = locations.map((location) => {
     .reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {})
 })
 
-ko.applyBindings(
-  bakeries
-)
+ko.applyBindings(bakeries)
+
+
+function titleSelector(obj) {
+  return obj && obj.title
+}
+
+function categorySelector(obj) {
+  return obj && obj.category.join(',')
+}
+
+const searchForString = (selector) => (searchString, location) => {
+  const selectedString = selector(location) // -> string
+  const re = RegExp(`(${searchString})`, 'g')
+  const matched = re.test(selectedString)
+  return (matched) ? location : null;
+}
+
+const searchForTitle = searchForString(titleSelector);
+
+function locationsFilter(searchParam, locations, searchAlgorithm = searchForTitle) {
+  return locations.filter((location) => (searchAlgorithm(searchParam, location)))
+}
+
+
 
