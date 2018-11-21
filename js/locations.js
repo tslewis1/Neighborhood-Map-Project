d13e8c474891e91dc8cf6fd2cb0d4fbf3d66c024
Taryn Lewis <tarynslewis@gmail.com>
2018-11-21 11:40:55 -0800

    Add the model file

---
+++ js/locations.js
@@ -0,0 +1,41 @@
+var locations = [
+  {
+    title: "Manresa Bread",
+    location: { lat: 37.227059, lng: -121.981004 },
+    category: ["bakery", "bread"]
+  },
+  {
+    title: "Fleur De Cocoa",
+    location: { lat: 37.223632, lng: -121.983657 },
+    category: ["bakery", "cafe"]
+  },
+  {
+    title: "Icing On The Cake",
+    location: { lat: 37.222009, lng: -121.982357 },
+    category: ["bakery", "cake"]
+  },
+  {
+    title: "Mama's Bakery",
+    location: { lat: 37.231465, lng: -121.980132 },
+    category: ["bakery"]
+  },
+  {
+    title: "Maple Leaf Donuts",
+    location: { lat: 37.233336, lng: -121.978076 },
+    category: ["bakery", "donuts"]
+  },
+  {
+    title: "Nothing Bundt Cakes",
+    location: { lat: 37.229968, lng: -121.981321 },
+    category: ["bakery", "cake"]
+  }
+];
+
+var Bakery = function(title, category) {
+  this.title = title;
+  this.category = category;
+}
+
+var viewModel = {
+  bakeries: locations.map(({title, category}) => (new Bakery (title, category)));
+}
